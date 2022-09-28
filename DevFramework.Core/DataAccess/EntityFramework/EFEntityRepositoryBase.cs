8aedbe3d34ea6ca3658cec7d3afd41c678d19925
eda.bezek <eda_bezek@hotmail.com>
2022-09-28 19:44:52 +0300

    EFramework and Quaryable Repository

---
+++ DevFramework.Core/DataAccess/EntityFramework/EFEntityRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EFEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public TEntity Add(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var addedEntity=context.Entry(entity);//eklenecek değeri değişkene atıp
+                addedEntity.State = EntityState.Added; //durumunu eklenecek olarak belirle 
+                context.SaveChanges();
+                return entity;//eklenmiş nesneyi döndürüyoruz
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);//silinecek değeri değişkene atıp
+                deletedEntity.State = EntityState.Deleted; //durumunu silinecek olarak belirle 
+                context.SaveChanges();
+                //silinen değeri döndürmeye gerek yok
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);//tek bir nesne döndüreceğiz.SingleOrDefault ile dizi içinden tek birtane gelecek.
+            }
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter)//filterdan gelen değer yok ise,ilgili TEntity'e Set ile DbContext'ine abone oluyoruz,filtre doluysa where şartına filterı gönderip çekiyoruz.
+        {
+            using (var context=new TContext())
+            {
+                return filter==null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();    
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);//değiştirilecek değeri değişkene atıp
+                updatedEntity.State = EntityState.Modified; //durumunu değişecek olarak belirle 
+                context.SaveChanges();
+                return entity;//değişmiş nesneyi döndürüyoruz
+            }
+        }
+    }
+}
---
+++ DevFramework.Core/DataAccess/EntityFramework/EFQueryableRepositoryBase.cs
@@ -0,0 +1,34 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EFQueryableRepositoryBase<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _context;
+        private IDbSet<T> _entities;
+        public EFQueryableRepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table => this.Entities; //tablo çağırıldığında entities döndürecek
+
+        protected virtual IDbSet<T> Entities //Baska sınıflar EFQueryableRepositoryBase'i inherit ederken kullanamasın diye protected yapıyoruz.
+        {
+            get 
+            {
+                if (_entities==null)
+                {
+                    _entities=_context.Set<T>();  //contexteki gelen t ye abone ol  
+                }
+                return _entities;
+            }
+        } 
+    }
+}
