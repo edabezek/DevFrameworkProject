d2f66a64b31d2f723e1383eb060106141e89f041
eda.bezek <eda_bezek@hotmail.com>
2022-10-07 12:52:17 +0300

    Validation Ninject impelementation

---
+++ DevFramework.Northwind.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,21 @@
+﻿using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
+using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.DependencyResolvers.Ninject
+{
+    public class ValidationModule : NinjectModule
+    {
+        //clientside validation yaptığımızda çalıştığımız nesneyi görüp o nesneye göre validator instance'ı isteyebiliriz.
+        public override void Load()
+        {
+            Bind<IValidator<Product>>().To<ProductValidator>().InSingletonScope();
+        }
+    }
+}
